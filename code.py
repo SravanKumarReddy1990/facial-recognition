7001670b04de1f188952d6fc9b1860db6b9e58ec
sravan <bskreddy1990@gmail.com>
2019-05-11 13:46:13 +0530

    First commit

--- code.py
+++ code.py
@@ -1,4 +1,12 @@
-def index(request):
-    r = "vjdbvsjbd"
-    print(r)
-    return HttpResponse('<pre>' + r + '</pre>')
+import web
+urls = (
+'/input', 'index'
+)
+class index:
+    def GET(self):
+        i = web.input(name=None)
+        return render.index(i.name)
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+render = web.template.render('examples/')
