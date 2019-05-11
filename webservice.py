8e76ee119ebc25a116759c11db664c842fed06b2
sravan <bskreddy1990@gmail.com>
2019-05-11 23:29:11 +0530

    make it better

--- webservice.py
+++ webservice.py
@@ -99,7 +99,7 @@ def detect_faces_in_image(file_stream,train_dir, model_save_path=None, n_neighbo
                 }
                 #users = json.dumps(result)
                 s1.append(result)
-    return jsonify(json.dumps(s1))
+    return jsonify(results=json.dumps(s1))
 
 if __name__ == "__main__":
     app.run(use_reloader=True, debug=True)
