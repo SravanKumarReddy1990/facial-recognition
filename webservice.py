46b50d5fd21c66a877d1326ae45dd4760d0c1e65
sravan <bskreddy1990@gmail.com>
2019-05-11 19:28:05 +0530

    make it better

--- webservice.py
+++ webservice.py
@@ -18,8 +18,8 @@
 
 import face_recognition
 from flask import Flask, jsonify, request, redirect
-from .facerecognitionknn import train
-from .facerecognitionknn import predict
+from ..face_recognition_knn import train
+from ..face_recognition_knn import predict
 
 # You can change this to any folder on your system
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
