182b24cf7ef58bb9bfc7f03ca0f025e95593fee7
sravan <bskreddy1990@gmail.com>
2019-05-11 19:49:00 +0530

    make it better

--- webservice.py
+++ webservice.py
@@ -56,14 +56,14 @@ def upload_image():
             print("Training complete!")
 
             # STEP 2: Using the trained classifier, make predictions for unknown images
-            for image_file in os.listdir("knn_examples/test"):
-                full_file_path = os.path.join("knn_examples/test", image_file)
+            #for image_file in os.listdir("knn_examples/test"):
+            #full_file_path = os.path.join("knn_examples/test", image_file)
 
             print("Looking for faces in {}".format(image_file))
 
             # Find all people in the image using a trained classifier model
             # Note: You can pass in either a classifier file name or a classifier model instance
-            predictions = predict(full_file_path, model_path="trained_knn_model.clf")
+            predictions = predict(file, model_path="trained_knn_model.clf")
 
             # Print results on the console
             for name, (top, right, bottom, left) in predictions:
