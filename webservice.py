95afacab7111db02d7331927add9cc0738b2371b
sravan <bskreddy1990@gmail.com>
2019-05-12 13:34:05 +0530

    make it better

--- webservice.py
+++ webservice.py
@@ -68,6 +68,8 @@ def upload_image():
 def detect_faces_in_image(file_stream,train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
     
     s1=[]
+    X = []
+    y = []
     for class_dir in os.listdir(train_dir):
         if not os.path.isdir(os.path.join(train_dir, class_dir)):
             continue
@@ -77,26 +79,51 @@ def detect_faces_in_image(file_stream,train_dir, model_save_path=None, n_neighbo
         for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
             image = face_recognition.load_image_file(img_path)
             face_bounding_boxes = face_recognition.face_encodings(image)
+            X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
+            y.append(class_dir)
+            X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
+            y.append(class_dir)
+            knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
+            knn_clf.fit(X, y)
+            # Save the trained KNN classifier
+            if model_save_path is not None:
+                with open(model_save_path, 'wb') as f:
+                    pickle.dump(knn_clf, f) 
 
             # Load the uploaded image file
-            img = face_recognition.load_image_file(file_stream)
+            #img = face_recognition.load_image_file(file_stream)
             # Get face encodings for any faces in the uploaded image
-            unknown_face_encodings = face_recognition.face_encodings(img)
+            #unknown_face_encodings = face_recognition.face_encodings(img)
+            #if knn_clf is None:
+            #    with open(model_save_path, 'rb') as f:
+            #       knn_clf = pickle.load(f)
 
-            face_found = False
-            is_obama = False
+            # Load image file and find face locations
+            X_img = face_recognition.load_image_file(file_stream)
+            X_face_locations = face_recognition.face_locations(X_img)
 
-            #if len(unknown_face_encodings) > 0:
-            face_found = True
-            # See if the first face in the uploaded image matches the known face of Obama
-            match_results = face_recognition.compare_faces([face_bounding_boxes], unknown_face_encodings)
+            # If no faces are found in the image, return an empty result.
+            if len(X_face_locations) == 0:
+                return []
+
+            # Find encodings for faces in the test iamge
+            faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+
+            # Use the KNN model to find the best matches for the test face
+            closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+            are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+            pridictions= [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+            for name, (top, right, bottom, left) in predictions:
+                print("- Found {} at ({}, {})".format(name, left, top))
+
+            #match_results = face_recognition.compare_faces([face_bounding_boxes], unknown_face_encodings[0])
             #print(match_results[0])
-            if (match_results[0]==True).all():
-                is_obama = True
+            #if (match_results[0]==True).all():
+                #is_obama = True
                 # Return the result as json
                 result = {
-                    "face_found_in_image": face_found,
-                    "is_picture_of": img_path
+                    #"face_found_in_image": face_found,
+                    "is_picture_of": "- Found {} at ({}, {})".format(name, left, top)
                 }
                 #users = json.dumps(result)
                 s1.append(result)
