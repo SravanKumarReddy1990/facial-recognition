49e297f043aa31a6f32ad8ebcbc0ce78e8f574eb
sravan <bskreddy1990@gmail.com>
2019-05-12 09:46:33 +0530

    make it better

--- webservice.py
+++ webservice.py
@@ -90,6 +90,7 @@ def detect_faces_in_image(file_stream,train_dir, model_save_path=None, n_neighbo
             face_found = True
             # See if the first face in the uploaded image matches the known face of Obama
             match_results = face_recognition.compare_faces([unknown_face_encodings], face_bounding_boxes[0])
+            print(match_results[0])
             if match_results[0]:
                 is_obama = True
                 # Return the result as json
