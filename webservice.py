648d6166d0d78063a468bbce824608b219f7f769
sravan <bskreddy1990@gmail.com>
2019-05-12 18:45:38 +0530

    make it better

--- webservice.py
+++ webservice.py
@@ -84,8 +84,9 @@ def upload_image():
                      }
                      #users = json.dumps(result)
                      s1.append(result)
-	
-             return jsonify(s1)
+             for item in s1: 
+                 full_filename=item["face_found_in_image"]
+                 return render_template("index.html", user_image = full_filename)
              #return detect_faces_in_image(file,"knn_examples/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
 
     # If no valid image file was uploaded, show the file upload form:
