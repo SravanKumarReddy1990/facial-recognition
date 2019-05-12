d2602bcad05cbb91446a4c8234a50a49e6b220bb
sravan <bskreddy1990@gmail.com>
2019-05-12 14:55:20 +0530

    make it better

--- webservice.py
+++ webservice.py
@@ -52,7 +52,7 @@ def upload_image():
 
         if file and allowed_file(file.filename):
              print("Training KNN classifier...")
-             classifier = train("knn_examples/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
+             classifier = train(file,"knn_examples/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
              print("Training complete!")
              s1=[]
              # STEP 2: Using the trained classifier, make predictions for unknown images
@@ -91,29 +91,29 @@ def upload_image():
     </form>
     '''
 
-def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
+def train(imgfile,train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
     
     X = []
     y = []
 
     # Loop through each person in the training set
-    for class_dir in os.listdir(train_dir):
-        if not os.path.isdir(os.path.join(train_dir, class_dir)):
-            continue
+    #for class_dir in os.listdir(train_dir):
+    #    if not os.path.isdir(os.path.join(train_dir, class_dir)):
+    #        continue
 
         # Loop through each training image for the current person
-        for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
-            image = face_recognition.load_image_file(img_path)
-            face_bounding_boxes = face_recognition.face_locations(image)
+    #    for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
+    image = face_recognition.load_image_file(img_path)
+    face_bounding_boxes = face_recognition.face_locations(image)
 
-            if len(face_bounding_boxes) != 1:
-                # If there are no people (or too many people) in a training image, skip the image.
-                if verbose:
-                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
-            else:
-                # Add face encoding for current image to the training set
-                X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
-                y.append(class_dir)
+    if len(face_bounding_boxes) != 1:
+        # If there are no people (or too many people) in a training image, skip the image.
+        if verbose:
+            print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+        else:
+            # Add face encoding for current image to the training set
+            X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
+            y.append(class_dir)
 
     # Determine how many neighbors to use for weighting in the KNN classifier
     if n_neighbors is None:
